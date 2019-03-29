0442b4cfe6d77e7c50846f842ab1657768dd81e4
SaneYuki-m <s.makino@fuji.waseda.jp>
2019-03-29 22:59:03 +0900

    [機能追加]PowerUpScene作成
    PowerUpScene作成

---
+++ MyMonsterts/Assets/Scripts/MonsterScripts/PowerUpScene/PowerUpButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerUpButton : MonoBehaviour {
+
+	[SerializeField] private Text powerUpConfirmText;
+	
+	public void PowerUpButtonClicked(){
+		
+	}
+}
