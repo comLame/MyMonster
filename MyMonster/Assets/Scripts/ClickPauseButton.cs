b4baafccd2099e23aa9921e58637ef6eb3285242
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2019-05-07 17:29:56 +0900

    機能修正　LeargeFileのプッシュ

---
+++ MyMonster/Assets/Scripts/ClickPauseButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickPauseButton : MonoBehaviour {
+
+	private bool isPause = false;
+
+	public void OnClick(){
+		if(isPause){
+			//動かす
+			//Time.timeScale = 1;
+			//iTween.Resume();
+
+			isPause = !isPause;
+		}else{
+			//止める
+			//Time.timeScale = 0;
+			//iTween.Pause();
+
+			isPause = !isPause;
+		}
+	}
+}
