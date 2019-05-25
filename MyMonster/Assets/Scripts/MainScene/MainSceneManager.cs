2438ea1fba3bfd9f34b767ed85290f687ed8e580
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2019-05-25 23:36:28 +0900

    機能追加　タブ遷移時のアニメーション追加

---
+++ MyMonster/Assets/Scripts/MainScene/MainSceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSceneManager : MonoBehaviour {
+
+	//オブジェクト参照
+	public GameObject canvasHome;
+	public GameObject canvasMonster;
+	public GameObject canvasShop;
+	public GameObject canvasGatya;
+	public GameObject canvasSetting;
+
+	private List<GameObject> canvasArray = new List<GameObject>();
+
+	private void Start(){
+		canvasArray.Add(canvasHome);
+		canvasArray.Add(canvasMonster);
+		canvasArray.Add(canvasShop);
+		canvasArray.Add(canvasGatya);
+		canvasArray.Add(canvasSetting);
+
+		int num_canvas = canvasArray.Count;
+		for(int i=0;i<num_canvas;i++){
+			canvasArray[i].SetActive(true);
+			if(i != 0)canvasArray[i].SetActive(false);
+		}
+	}
+}
---
+++ MyMonster/Assets/Scripts/MainScene/MoveCategorize.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCategorize : MonoBehaviour {
+
+	public List<GameObject> left = new List<GameObject>();
+	public List<GameObject> right = new List<GameObject>();
+}
