46d71137b11e798db3c6669e17f1275335bb8c8a
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2019-06-24 20:03:30 +0900

    機能追加　画面作成、データ保存、アイコン自動作成

---
+++ MyMonster/Assets/Scripts/General/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameManager : MonoBehaviour {
+
+	public List<Monster> ownMonsters = new List<Monster>();
+
+	private void Start(){
+		DataSet();
+		
+	}
+
+	private void DataSet(){
+		for(int j=0;j<3;j++){
+			for(int i=0;i<10;i++){
+				Monster mons = new Monster();
+				mons.No = j+1;
+				mons.uniqueID = (j * 10) + i + 1;
+				mons.exp = Random.Range(0,51);
+				mons.level = Random.Range(5,31);
+				ownMonsters.Add(mons);
+			}
+		}
+
+		SaveData.SetList<Monster>("ownMonsters",ownMonsters);
+		SaveData.Save();
+	}
+
+	private void Update(){
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			//セーブデータの取得
+			List<Monster> getOwnMonsters = SaveData.GetList<Monster>("ownMonsters",new List<Monster>());
+			int test = SaveData.GetInt("test",0);
+			Debug.Log(JsonUtility.ToJson(getOwnMonsters[1]));
+			//Debug.Log(test);
+
+        }
+	}
+}
