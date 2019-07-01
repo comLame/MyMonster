ab870266f252ddf84ee45dbb55adeca6312a4538
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2019-07-01 17:59:41 +0900

    機能追加　レベルアップ実装

---
+++ MyMonster/Assets/Scripts/ResultScene/LevelUpSliderAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUpSliderAnimation : MonoBehaviour {
+
+	public void StartAnimation(int start,int goal,float time){
+		iTween.ValueTo(gameObject,iTween.Hash("from",start,"to",goal,"onupdate","UpdateAnimation",
+			"onupdatetarget",gameObject,"time",time));
+	}
+
+	private void UpdateAnimation(float value){
+		gameObject.GetComponent<Slider>().value = value;
+	}
+}
